52abe3d8ed7c76a98672d9239f5c0f60b152e5f3
Antoine Gersant <antoine.gersant@lesforges.org>
2015-01-25 13:31:14 -0800

    Test script can now skip unreadable files or files without ID3v2 tags

--- src/format/id3v2/Main.hx
+++ src/format/id3v2/Main.hx
@@ -1,10 +1,11 @@
 package format.id3v2;
 
+import format.id3v2.Data.ParseError;
 import format.id3v2.Reader;
 import haxe.io.Path;
-import haxe.macro.Format;
 import sys.FileSystem;
 import sys.io.File;
+import sys.io.FileInput;
 
 /**
  * ...
@@ -18,12 +19,42 @@ class Main
 	{
 		for (fileName in FileSystem.readDirectory(Sys.getCwd()))
 		{
+			var file : FileInput;
 			var path = new Path(fileName);
+			
+			try {
+				if (FileSystem.isDirectory(path.toString()))
+					continue;
+			}
+			catch (d : Dynamic)
+			{
+				if (d == "std@sys_file_type")
+				{
+					Sys.println("Cannot open: " + fileName);
+					continue;
+				}
+				throw d;
+			}
+			
 			if (path.ext != "mp3")
 				continue;
-			trace(path);
-			var file = File.read(path.toString(), true);
-			var id3v2 = new Reader(file);
+			
+			file = File.read(path.toString(), true);
+			try
+			{
+				trace("Analyzing " + fileName);
+				var id3v2 = new Reader(file);
+			}
+			catch (e : ParseError)
+			{
+				if (e == ParseError.INVALID_HEADER_FILE_IDENTIFIER)
+				{
+					Sys.println("No ID3v2 tag: " + fileName);
+					continue;
+				}
+				throw e;
+			}
+			file.close();
 		}
 		
 	}
